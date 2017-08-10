cd3d2b4c9850273054f63d61c15b8ae4627a4350
stockcj <claudestockton@gmail.com>
2017-08-10 17:12:14 +0100

    Day 7 part 4.1

--- src/app/contingency-management/exam-item/exam-item.component.ts
+++ src/app/contingency-management/exam-item/exam-item.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { Exam } from '../exam';
+import { ExamComponent} from '../examcomponent';
 
 @Component({
   selector: 'ct-exam-item',
